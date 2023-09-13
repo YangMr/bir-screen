0b16e75545ce6aabfab07127e17a04f235f8c077
YangMr <759168442@qq.com>
2023-09-13 11:10:54 +0800

    feat(park): 实现园区概况

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import BigScreen from '../views/BigScreen/index.vue'
+
+const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/big-screen'
+    },
+    {
+      path: '/big-screen',
+      name: 'big-screen',
+      component: BigScreen
+    }
+  ]
+})
+
+export default router
